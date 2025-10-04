079355422a00a8d47c55bf015c60a3b762a41635
RoseEyesTwilightDragon <matthew@kovacssheppard.com>
2025-10-04 22:07:18 +0100

    Create App.tsx

---
+++ brand/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import BrandTimeline from '@/components/BrandTimeline'
+
+export default function App() {
+  return (
+    <div className="min-h-screen">
+      <BrandTimeline />
+    </div>
+  )
+}

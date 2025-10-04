7dbac6d37917f83bde5eadbdbf63c0c45125d154
RoseEyesTwilightDragon <matthew@kovacssheppard.com>
2025-10-04 22:11:05 +0100

    Create badge.tsx

---
+++ brand/src/components/ui/badge.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import clsx from 'clsx'
+
+export function Badge({ className, ...props }: React.HTMLAttributes<HTMLSpanElement>) {
+  return <span className={clsx('inline-flex items-center rounded-full border border-border bg-muted/50 px-2 py-0.5 text-xs', className)} {...props}/>
+}
